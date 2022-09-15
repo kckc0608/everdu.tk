d0cef22594fe166f16d0edc12e2fb8ca3f45a7dc
kwon <kckc0608@naver.com>
2022-09-15 14:37:30 +0900

    HOTFIX : 모바일에서 폰트 사이즈가 다르게 보이는 문제 수정
    뷰포트를 설정하지 않아서 모바일에서는 사이트의 폰트가 제각각으로 보였습니다.
    뷰포트를 설정하여 똑같은 폰트 사이즈를 유지할 수 있도록 수정하였습니다.

--- script/calc_date.js
+++ script/calc_date.js
@@ -1,25 +1,23 @@
-var target = document.querySelector('#military_end_date');
-var target2 = document.querySelector('#military_end_percent');
+var target = document.querySelector('#military_days');
+var target2 = document.querySelector('#military_percent');
 const start_date = new Date('2021-09-06');
 const end_date = new Date('2023-06-05');
 
-function updateMilDate() {
+console.log(target);
+console.log(target2);
+
+function updateMilitaryInfo() {
     let today = new Date();
     let periodMilSec = end_date.getTime() - today.getTime();
-    let left_time = periodMilSec / (1000*60*60*24);
-    target.innerText = left_time.toFixed(6);
-}
-
-function updateMilitaryPercent() {
-    let today = new Date();
-    let periodMilSec = today.getTime() - start_date.getTime();
     let periodWholeMilSec = end_date.getTime() - start_date.getTime();
-    let percentage = (periodMilSec / periodWholeMilSec)*100
-    target2.innerText = percentage.toFixed(6);
+
+    let percentage = ((periodMilSec / periodWholeMilSec)*100).toFixed(6);
+    let left_time = (periodMilSec / (1000*60*60*24)).toFixed(6);
+
+    target.innerHTML = left_time;
+    target2.innerHTML = percentage;
 }
 
-updateMilDate();
-updateMilitaryPercent();
+updateMilitaryInfo();
 
-setInterval(updateMilDate, 1);
-setInterval(updateMilitaryPercent, 1);
+setInterval(updateMilitaryInfo, 10);
